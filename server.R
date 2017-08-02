d741026d8c4899edb24d5747c2127f99492d0348
Yufree <yufree@live.cn>
2017-08-02 12:46:25 -0400

    update

--- server.R
+++ server.R
@@ -1,4 +1,5 @@
 
+
 # This is the server logic for a Shiny web application.
 # You can find out more about building applications with Shiny here:
 #
@@ -6,21 +7,56 @@
 #
 
 library(shiny)
-library(enviGCMS)
+source('script.R')
 
-LoadToEnvironment <- function(RData, env=new.env()) {
+LoadToEnvironment <- function(RData, env = new.env()) {
         load(RData, env)
         return(env)
 }
 
-shinyServer(function(input, output,session) {
+shinyServer(function(input, output, session) {
         dataInput <- reactive({
                 sessionEnvir <- sys.frame()
-                if (!is.null(input$file)) load(input$file$datapath, sessionEnvir)
+                if (!is.null(input$file))
+                        load(input$file$datapath, sessionEnvir)
         })
         
-        output$plot <- renderPlot({
-                if (is.null(dataInput()))  return()  else plotmr(xset3)
+        dataInput2 <- reactive({
+                if (!is.null(input$file2)) {
+                        read.csv(input$file2)
+                }
         })
-
+        
+        output$plot1 <- renderPlot({
+                if (is.null(dataInput()))
+                        return()
+                else
+                        plotmr(xset,
+                               rsdcf = input$rsd,
+                               inscf = input$ins)
+        })
+        
+        output$plot2 <- renderPlot({
+                if (is.null(dataInput()))
+                        return()
+                else
+                        plotmrc(xset,
+                                rsdcf = input$rsd,
+                                inscf = input$ins)
+        })
+        
+        output$plot3 <- renderPlot({
+                if (is.null(dataInput()))
+                        return()
+                else
+                        plotrsd(xset)
+        })
+        
+        output$plot4 <- renderPlot({
+                if (is.null(dataInput()))
+                        return()
+                else
+                        plotpca(xset)
+        })
+        
 })
--- ui.R
+++ ui.R
@@ -1,4 +1,6 @@
 
+
+
 # This is the user-interface definition of a Shiny web application.
 # You can find out more about building applications with Shiny here:
 #
@@ -8,28 +10,67 @@
 library(shiny)
 
 shinyUI(fluidPage(
-
-  # Application title
-  titlePanel("Visualization based on xcms"),
-
-  h4('Uploading Files'),
-  fileInput('file',
-            label = 'R Dataset with xcmsSet object in it',
-            accept = c('.RData')),
-  
-  # Sidebar with a slider input for rsd
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("rsd",
-                  "RSD(%)",
-                  min = 1,
-                  max = 100,
-                  value = 30)
-    ),
-
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("plot")
-    )
-  )
+        # Application title
+        titlePanel("Visualization based on xcms"),
+        
+        # Sidebar with a slider input for rsd and ins
+        sidebarLayout(
+                sidebarPanel(
+                        h4('Uploading Files'),
+                        fileInput('file',
+                                  label = 'R Dataset with xcmsSet object in it',
+                                  accept = c('.RData')),
+                        # h4('Uploading csv Files'),
+                        # fileInput('file2',
+                        #           label = 'R Dataset in csv format',
+                        #           accept = c('.csv')),
+                        # 
+                        sliderInput(
+                                "rsd",
+                                "RSD(%)",
+                                min = 1,
+                                max = 100,
+                                value = 30
+                        ),
+                        sliderInput(
+                                "ins",
+                                "Intensity",
+                                min = 1,
+                                max = 10,
+                                value = 5
+                        )
+                ),
+                
+                # Show a plot of the generated distribution
+                mainPanel(
+                        tabsetPanel(type = "tabs",
+                                    tabPanel('Description',p("This app is developed for xcmsSet object data visualization."),
+                                             h4("Usage"),
+                                             br(),
+                                             p("You need to process data locally with xcms and upload the xcmsSet object to this app. The following code would help:"),
+                                             br(),
+                                             code("library(xcms)",br(),
+                                                     "library(enviGCMS)",br(),
+                                                     "path <- './data'",br(),
+                                                     "xset <- getdata(path)",br(),
+                                                     "save(xset,file = 'xset.RData')",br()
+                                             ),
+                                             br(),
+                                             p("Then just upload your 'xset.RData' to this app."),
+                                             br(),
+                                             p("You could download demo data", a("here",href = "")),
+                                             "Contact me by click",
+                                             a("here", href = "mailto:yufreecas@gmail.com"),
+                                             'or just add an issue on',
+                                             a("Github",href = "https://github.com/yufree/xcmsplus")
+                                    ),
+                        tabPanel("Peaks",            
+                        plotOutput("plot1"),
+                        plotOutput("plot2")),
+                        tabPanel("RSD",
+                        plotOutput("plot3")),
+                        tabPanel("PCA",
+                        plotOutput("plot4"))
+                )
+        ))
 ))
